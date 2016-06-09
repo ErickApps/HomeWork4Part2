83c1419cb50be2a7773eb1aa8946354e68d8f793
ErickApps <pitjitsu@hotmail.com>
2016-06-08 20:29:06 -0700

    Add files via upload
    all working

---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HomeWork4Part2
+{
+	public partial class MainPage : ContentPage
+	{
+		List<string> phraseList = new List<string>();
+		public MainPage ()
+		{
+			InitializeComponent ();
+
+			phraseList.Add("I want to seeSharp");
+			phraseList.Add("Im starting to like Xamarin");
+			phraseList.Add("But I still like Xcode better");
+			phraseList.Add("but xamarin I can build in three platform at the same time");
+
+
+		}
+		async void OnClickedRandom(object sender, EventArgs e){
+			var random = new Random();
+			displayLabel.Text = this.phraseList [random.Next (0, 3)];
+		}
+	}
+}
+
